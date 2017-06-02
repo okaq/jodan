3f23e247e3400b4d360198504e7731a43648a440
AQ <aq@okaq.com>
2017-06-02 13:53:10 +0000

    Push server test handler

--- web.go
+++ web.go
@@ -17,9 +17,15 @@ func WebHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w,r,INDEX)
 }
 
+func PushHandler(w http.ResponseWriter, r *http.Request) {
+	// test for http/2 push server
+	w.Write([]byte("ok push!"))
+}
+
 func main() {
 	fmt.Println("web server started on localhost:80")
 	fmt.Printf("start time: %s\n.", time.Now().String())
 	http.HandleFunc("/", WebHandler)
+	http.HandleFunc("/a", PushHandler)
 	http.ListenAndServe(":80", nil)
 }
